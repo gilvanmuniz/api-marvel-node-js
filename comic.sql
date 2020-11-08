9e7383fdcd71ae0d6bdd8c9ac951686b489ed71d
gilvan.muniz <camisetaecia@live.com>
2020-11-08 10:42:56 -0300

    pronto path characters, characters/id e characters/id/comics

---
+++ comic.sql
@@ -0,0 +1,36 @@
+use heroes;
+CREATE TABLE `comic` (
+    `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `digitalId` int,
+    `title` varchar(60) ,
+    `issueNumber` int,
+    `variantDescription` varchar(150) ,
+    `description` varchar(150),
+    `modified` Date,
+    `isbn` varchar(150),
+    `upc` varchar(150),
+    `diamondCode` varchar(150),
+    `ean` varchar(150),
+    `issn` varchar(150),
+    `format` varchar(150),
+    `pageCount` int,
+    `resourceURI` varchar(150),
+	`comics_id` bigint,
+    FOREIGN KEY (`comics_id`) REFERENCES caractere (`heroes_id`)   
+);
+
+/* campos para relacionamentos 
+textObjects (Array[TextObject], optional): A set of descriptive text blurbs for the comic.,
+urls (Array[Url], optional): A set of public web site URLs for the resource.,
+series (SeriesSummary, optional): A summary representation of the series to which this comic belongs.,
+variants (Array[ComicSummary], optional): A list of variant issues for this comic (includes the "original" issue if the current issue is a variant).,
+collections (Array[ComicSummary], optional): A list of collections which include this comic (will generally be empty if the comic's format is a collection).,'
+collectedIssues (Array[ComicSummary], optional): A list of issues collected in this comic (will generally be empty for periodical formats such as "comic" or "magazine").,
+dates (Array[ComicDate], optional): A list of key dates for this comic.,
+prices (Array[ComicPrice], optional): A list of prices for this comic.,
+thumbnail (Image, optional): The representative image for this comic.,
+images (Array[Image], optional): A list of promotional images associated with this comic.,
+creators (CreatorList, optional): A resource list containing the creators associated with this comic.,
+characters (CharacterList, optional): A resource list containing the characters which appear in this comic.,
+stories (StoryList, optional): A resource list containing the stories which appear in this comic.,
+events (EventList, optional): A resource list containing the events in which this comic appears. */
--- routes/characteres-route.js
+++ routes/characteres-route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
 
-router.get('/',(req, res, next) => {
+router.get('/', (req, res, next) => {
     mysql.getConnection((error, coon) => {
         coon.query(
             `SELECT *                       
@@ -12,162 +12,249 @@ router.get('/',(req, res, next) => {
             INNER JOIN comiclist ON (caractere.comic_id = comiclist.id)            
             INNER JOIN storylist ON (caractere.storiy_id = storylist.id)
             INNER JOIN url ON (caractere.url_id = url.id) `,
-            (error, result, field) =>{
-                if(error){
+            (error, result, field) => {
+                if (error) {
                     return res.status(500).send({
-                        error:error,
-                        response:null
+                        error: error,
+                        response: null
                     });
                 }
-                const tamando = result.length;              
-                const response =  result.map(resultado => {                           
-                          return {
-                              code:200,
-                              status:"ok",
-                              copyright:resultado.copyright,
-                              attributionText:resultado.attributionText,
-                              attributionHTML:resultado.attributionHTML,
-                              etag:resultado.etag,
-                              data:{
-                                offset: 0,
-                                limit: tamando,
-                                total: 1493,
-                                count: tamando,
-                                results: [
-                                    {
-                                        id:resultado.id,
-                                        name:resultado.name,
-                                        description:resultado.description,
-                                        modified:resultado.modified,
-                                        thumbnail:{
-                                            path:resultado.path,
-                                            extension:resultado.extension
-                                        },
-                                        events:{                                            
-                                            available:resultado.available_event,
-                                            collectionuri:resultado.collectionuri_event,
-                                            returned:resultado.returned_event
-                                        },
-                                        serie:{
-                                            available:resultado.available,
-                                            collectionuri:resultado.collectionuri,
-                                            returned:resultado.returned
-                                        },
-                                        comics:{
-                                            available:resultado.available_comic,
-                                            collectionuri:resultado.collectionuri_comic,
-                                            returned:resultado.returned_comic
-                                        },
-                                        urls:{
-                                            type:resultado.type,
-                                            url:resultado.url
-                                        }
+                const tamando = result.length;
+                const response = result.map(resultado => {
+                    return {
+                        code: 200,
+                        status: "ok",
+                        copyright: resultado.copyright,
+                        attributionText: resultado.attributionText,
+                        attributionHTML: resultado.attributionHTML,
+                        etag: resultado.etag,
+                        data: {
+                            offset: 0,
+                            limit: tamando,
+                            total: 1493,
+                            count: tamando,
+                            results: [
+                                {
+                                    id: resultado.heroes_id,
+                                    name: resultado.name,
+                                    description: resultado.description,
+                                    modified: resultado.modified,
+                                    thumbnail: {
+                                        path: resultado.path,
+                                        extension: resultado.extension
                                     },
-                                    
-                                    
-                                ],
-                              }
+                                    events: {
+                                        available: resultado.available_event,
+                                        collectionuri: resultado.collectionuri_event,
+                                        returned: resultado.returned_event
+                                    },
+                                    serie: {
+                                        available: resultado.available,
+                                        collectionuri: resultado.collectionuri,
+                                        returned: resultado.returned
+                                    },
+                                    comics: {
+                                        available: resultado.available_comic,
+                                        collectionuri: resultado.collectionuri_comic,
+                                        returned: resultado.returned_comic
+                                    },
+                                    urls: {
+                                        type: resultado.type,
+                                        url: resultado.url
+                                    }
+                                },
 
-                          }
-                      })    
-                
+
+                            ],
+                        }
+
+                    }
+                })
+
                 res.status(201).send({
-                    mensagem:'retorno positivo',
+                    mensagem: 'retorno positivo',
                     response
                 })
             }
         )
-    });    
+    });
 });
 
-router.get('/:id',(req, res, next) => {
-    
+router.get('/:id', (req, res, next) => {
+    console.log(req.params.id)
     mysql.getConnection((error, coon) => {
         coon.query(
             `SELECT *                       
-            FROM caractere WHERE caractere.id = ?             
-            INNER JOIN images ON (caractere.img_id = images.id)
-            INNER JOIN eventolista ON (caractere.event_id = eventolista.id)
-            INNER JOIN comiclist ON (caractere.comic_id = comiclist.id)            
-            INNER JOIN storylist ON (caractere.storiy_id = storylist.id)
-            INNER JOIN url ON (caractere.url_id = url.id) `,
-            [req.body.id],
-            (error, result, field) =>{
-                if(error){
+            FROM caractere             
+                INNER JOIN images ON (caractere.img_id = images.id)
+                INNER JOIN eventolista ON (caractere.event_id = eventolista.id)
+                INNER JOIN comiclist ON (caractere.comic_id = comiclist.id)            
+                INNER JOIN storylist ON (caractere.storiy_id = storylist.id)
+                INNER JOIN url ON (caractere.url_id = url.id)    
+            WHERE caractere.heroes_id = ?`,
+            [req.params.id],
+            (error, result, field) => {
+                if (error) {
                     return res.status(500).send({
-                        error:error,
-                        response:null
+                        error: error,
+                        response: null
                     });
                 }
-                const tamando = result.length;              
-                const response =  result.map(resultado => {                           
-                          return {
-                              code:200,
-                              status:"ok",
-                              copyright:resultado.copyright,
-                              attributionText:resultado.attributionText,
-                              attributionHTML:resultado.attributionHTML,
-                              etag:resultado.etag,
-                              data:{
-                                offset: 0,
-                                limit: tamando,
-                                total: 1493,
-                                count: tamando,
-                                results: [
-                                    {
-                                        id:resultado.id,
-                                        name:resultado.name,
-                                        description:resultado.description,
-                                        modified:resultado.modified,
-                                        thumbnail:{
-                                            path:resultado.path,
-                                            extension:resultado.extension
-                                        },
-                                        events:{                                            
-                                            available:resultado.available_event,
-                                            collectionuri:resultado.collectionuri_event,
-                                            returned:resultado.returned_event
-                                        },
-                                        serie:{
-                                            available:resultado.available,
-                                            collectionuri:resultado.collectionuri,
-                                            returned:resultado.returned
-                                        },
-                                        comics:{
-                                            available:resultado.available_comic,
-                                            collectionuri:resultado.collectionuri_comic,
-                                            returned:resultado.returned_comic
-                                        },
-                                        urls:{
-                                            type:resultado.type,
-                                            url:resultado.url
-                                        }
+                const tamando = result.length;
+                const response = result.map(resultado => {
+                    return {
+                        code: 200,
+                        status: "ok",
+                        copyright: resultado.copyright,
+                        attributionText: resultado.attributionText,
+                        attributionHTML: resultado.attributionHTML,
+                        etag: resultado.etag,
+                        data: {
+                            offset: 0,
+                            limit: tamando,
+                            total: 1493,
+                            count: tamando,
+                            results: [
+                                {
+                                    id: resultado.heroes_id,
+                                    name: resultado.name,
+                                    description: resultado.description,
+                                    modified: resultado.modified,
+                                    thumbnail: {
+                                        path: resultado.path,
+                                        extension: resultado.extension
                                     },
-                                    
-                                    
-                                ],
-                              }
+                                    events: {
+                                        available: resultado.available_event,
+                                        collectionuri: resultado.collectionuri_event,
+                                        returned: resultado.returned_event
+                                    },
+                                    serie: {
+                                        available: resultado.available,
+                                        collectionuri: resultado.collectionuri,
+                                        returned: resultado.returned
+                                    },
+                                    comics: {
+                                        available: resultado.available_comic,
+                                        collectionuri: resultado.collectionuri_comic,
+                                        returned: resultado.returned_comic
+                                    },
+                                    urls: {
+                                        type: resultado.type,
+                                        url: resultado.url
+                                    }
+                                },
 
-                          }
-                      })    
-                
+
+                            ],
+                        }
+
+                    }
+                })
+
                 res.status(201).send({
-                    mensagem:'retorno positivo',
+                    mensagem: 'retorno positivo',
                     response
                 })
             }
         )
-    });    
-    
+    });
+
 });
 
-router.post('/',(req, res, next) => {    
+router.get('/:id/comics', (req, res, next) => {
+    console.log(req.params.id)
+    mysql.getConnection((error, coon) => {
+        coon.query(
+            `SELECT *                       
+            FROM comic  WHERE comic.comics_id = ? 
+            `,   
+                      
+            [req.params.id],
+            (error, result, field) => {
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                const tamando = result.length;
+                const response = result.map(resultado => {
+                    return {
+                        code: 200,
+                        status: "ok",
+                        copyright: resultado.copyright,
+                        attributionText: resultado.attributionText,
+                        attributionHTML: resultado.attributionHTML,
+                        etag: resultado.etag,
+                        data: {
+                            offset: 0,
+                            limit: tamando,
+                            total: 1493,
+                            count: tamando,
+                            results: [
+                                {
+                                     id: resultado.id,
+                                     digitalId: resultado.digitalId,
+                                     title: resultado.title,                                     
+                                     issueNumber: 19,
+                                     variantDescription: resultado.variantDescription,
+                                     description: resultado.description,
+                                     modified: resultado.modified,
+                                     isbn:resultado.isbn,
+                                     upc:resultado.upc,
+                                     diamondCode: resultado.diamondCode,
+                                     ean: resultado.ean,
+                                     issn: resultado.issn,
+                                     format: resultado.format,
+                                     pageCount:resultado.pageCount,
+                                     resourceURI: resultado.resourceURI,
+                                     comics_id: resultado.comics_id,
+                                    events: {
+                                        available: resultado.available_event,
+                                        collectionuri: resultado.collectionuri_event,
+                                        returned: resultado.returned_event
+                                    },
+                                    serie: {
+                                        available: resultado.available,
+                                        collectionuri: resultado.collectionuri,
+                                        returned: resultado.returned
+                                    },
+                                    comics: {
+                                        available: resultado.available_comic,
+                                        collectionuri: resultado.collectionuri_comic,
+                                        returned: resultado.returned_comic
+                                    },
+                                    urls: {
+                                        type: resultado.type,
+                                        url: resultado.url
+                                    }
+                                },
+
+
+                            ],
+                        }
+
+                    }
+                })
+
+                res.status(201).send({
+                    mensagem: 'retorno positivo',
+                    response
+                })
+            }
+        )
+    });
+
+});
+
+router.post('/', (req, res, next) => {
     conn.query(
         'INSERT INTO caractere (Id, name) VALUES (?,?)',
         [req.body.productId, req.body.quantity],
         (error, result, field) => {
             conn.release();
-            if (error) { return res.status(500).send({ error: error }) }           
+            if (error) { return res.status(500).send({ error: error }) }
             return res.status(201).send(result);
         }
     )
